9a58b2ee4f6868de8285ec4ec4f82d32044085a6
gustavo-vasquez <vasquez.gustavo90@gmail.com>
2016-04-21 12:18:21 -0300

    Version inicial del proyecto.

---
+++ Logios/Logios/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Logios.MvcApplication" Language="C#" %>
---
+++ Logios/Logios/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Logios.Startup))]
+namespace Logios
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
