6dd99c537026577b21acef830610882fcbb0e789
Matias Fito <faito06@gmail.com>
2016-04-30 18:56:11 -0300

    Add Context y Actualizar Seed de los migraciones.

---
+++ Logios/Logios/Entities/LogiosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Logios.Entities
+{
+    public class LogiosContext : DbContext
+    {
+        public LogiosContext()
+                : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
--- Logios/Logios/Migrations/Configuration.cs
+++ Logios/Logios/Migrations/Configuration.cs
@@ -24,18 +24,24 @@ namespace Logios.Migrations
                 new Topic { Description = "Polinomios" }
             };
 
-            newTopics.ForEach(t => context.Topics.Add(t));
-            context.SaveChanges();
-
-            var newExercises = new List<Exercise>()
+            if (context.Topics.Count() == 0)
             {
-                new Exercise { Problem = "1+1", Development = "1+1=2", Solution = "2", Topic = newTopics[0]  },
-                new Exercise { Problem = "2+2", Development = "2+2=4", Solution = "4", Topic = newTopics[0] },
-                new Exercise { Problem = "2+2+2", Development = "2+2+2=6", Solution = "6", Topic = newTopics[1] }
-            };
+                newTopics.ForEach(t => context.Topics.AddOrUpdate(t));
+                context.SaveChanges();
+            }
 
-            newExercises.ForEach(e => context.Exercises.Add(e));
-            context.SaveChanges();
+            if (context.Exercises.Count() == 0)
+            {
+                var newExercises = new List<Exercise>()
+                {
+                    new Exercise { Problem = "1+1", Development = "1+1=2", Solution = "2", Topic = newTopics[0]  },
+                    new Exercise { Problem = "2+2", Development = "2+2=4", Solution = "4", Topic = newTopics[0] },
+                    new Exercise { Problem = "2+2+2", Development = "2+2+2=6", Solution = "6", Topic = newTopics[1] }
+                };
+
+                newExercises.ForEach(e => context.Exercises.AddOrUpdate(e));
+                context.SaveChanges();
+            }
         }
     }
 }
