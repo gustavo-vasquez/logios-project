74984da0c28482fe2abb32ac531d7d91aee52079
Matias Fito <faito06@gmail.com>
2016-05-15 15:29:31 -0300

    Pagina de Temas por Area Tematica con estilos, pero sin funcionalidad.

--- Logios/Logios/Services/TopicAreaService.cs
+++ Logios/Logios/Services/TopicAreaService.cs
@@ -35,7 +35,30 @@ namespace Logios.Services
                 }
             }
 
+            // Llenar con data falsa para ver como se muestran las cosas
+            result.TopicsByArea["Tema 3"] = this.GenerateFakeTopics(4);
+            result.TopicsByArea["Tema 4"] = this.GenerateFakeTopics(6);
+            result.TopicsByArea["Tema 5"] = this.GenerateFakeTopics(7);
+            result.TopicsByArea["Tema 6"] = this.GenerateFakeTopics(7);
+
             return result;
+        }
+
+        // Generar topics de mentira para ver el Layout, sacar cuando tengamos el feedbackd e Juan
+        private IEnumerable<Topic> GenerateFakeTopics(int topicCount)
+        {
+            var fakeTopics = new List<Topic>();
+
+            for (int i = 0; i < topicCount; i++)
+            {
+                fakeTopics.Add(new Topic()
+                {
+                    TopicId = 1,
+                    Description = string.Concat("Tema ", i)
+                });
+            }
+
+            return fakeTopics;
         }
     }
 }
