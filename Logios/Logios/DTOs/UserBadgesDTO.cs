488f304df176a2780024ac99466b34e227a4505d
axbll <axbll@BUELX067>
2016-05-29 22:30:18 -0300

    El usuario puede ver sus medallas y puntos discriminados

---
+++ Logios/Logios/DTOs/UserBadgesDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Logios.DTOs
+{
+    public class UserBadgesDTO
+    {
+        public int IdTrophy { get; set; }
+        public string DescriptionTrophy { get; set; }
+        public string ImageTrophy { get; set; }
+        public int TrophyPoints { get; set; }
+    }
+}
